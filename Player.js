a6e61d1f24795f42450d1bbf5f72f1e5f64d2a6d
Yunal Hodzha <113513080+YunalHodzha@users.noreply.github.com>
2023-08-15 15:18:31 +0300

    Implemented the player movement inside canvas

---
+++ Player.js
@@ -0,0 +1,13 @@
+export {player};
+
+const rocketImg = new Image();
+rocketImg.src = './images/rocket.png';
+
+const player = {
+    img: rocketImg,
+    x: 900,
+    y: 1100,
+    width: 180,
+    height: 150,
+    speed: 20,
+}
--- app.js
+++ app.js
@@ -1,25 +1,50 @@
+import { player } from "./player.js";
+
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 3000 / 1.5;
 const CANVAS_HEIGHT = canvas.height = 2000 / 1.5;
 
-const rocket = new Image();
-rocket.src = './images/rocket.png';
-rocket.width = 16;
-rocket.height = 16;
+const keyState = {};
+window.addEventListener("keydown", (event) => {
+    keyState[event.key] = true;
+});
+
+window.addEventListener("keyup", (event) => {
+    keyState[event.key] = false;
+})
+
 
 const backGroundImage = new Image();
 backGroundImage.src = './images/backGround.png'
 
-//ctx.drawImage(playerImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-
 
 function game() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(backGroundImage, 0, 0, 3000, 2000,0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(rocket, CANVAS_WIDTH /2 , CANVAS_HEIGHT / 1.2, 150, 150)
+    ctx.drawImage(backGroundImage, 0, 0, 3000, 2000, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    update();
+    ctx.drawImage(player.img, player.x, player.y, player.width, player.height)
 
 
+    function update() {
+        if (keyState["ArrowLeft"] && player.x > 0) {
+            player.x -= player.speed;
+        }
+
+        if (keyState["ArrowRight"] && player.x < CANVAS_WIDTH - player.width) {
+            player.x += player.speed;
+        }
+
+        if(keyState["ArrowUp"] && player.y > 0) {
+            player.y -= player.speed
+        }
+
+        if(keyState["ArrowDown"] && player.y < CANVAS_HEIGHT - player.height) {
+            player.y += player.speed
+        }
+    }
 }
 
-setInterval(game, 1000 / 1);
+setInterval(game, 1000 / 60);
+console.log("HEllo World")
